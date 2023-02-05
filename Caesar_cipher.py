e5afcced889ae1a128bdb3a89d77a1df94d624b9
kAgeNyx <57087618+Ankit-Kumar6@users.noreply.github.com>
2023-02-05 10:22:59 +0530

    Add files via upload

---
+++ Caesar_cipher.py
@@ -0,0 +1,51 @@
+import time
+
+def encrypt(message, shift):
+  result = ""
+  for char in message:
+    if char.isalpha():
+      shift_char = chr((ord(char.upper()) + shift - 65) % 26 + 65)  # char.upper is used to convert all the text to capital form, ord is used to change char to ascii number, and chr converts the ascii to alphabet.
+      if char.islower():
+        result += shift_char.lower()
+      else:
+        result += shift_char
+    else:
+      result += char
+  return result
+
+def decrypt(message, shift):
+  result = ""
+  for char in message:
+    if char.isalpha():
+      shift_char = chr((ord(char.upper()) - shift - 65) % 26 + 65)
+      if char.islower():
+        result += shift_char.lower()
+      else:
+        result += shift_char
+    else:
+      result += char
+  return result
+
+
+print("Welcome to caesar cipher!")
+time.sleep(1)
+print("Do you wish to Encrypt or Decrypt?")
+time.sleep(1)
+print('Enter 1 to "Encrypt" \n      2 to "Decrypt".')
+choice = int(input("Enter Your Choice: "))
+if choice != 1 and choice != 2:
+  print("Invalid choice.")
+  exit()
+time.sleep(1)
+message = input("Enter your message: ")
+time.sleep(1)
+shift = int(input("Enter your Key: "))
+time.sleep(1)
+
+if choice == 1:
+    result = encrypt(message, shift)
+    print(f"Your Encrypted message is {result}.")
+elif choice == 2:
+    result = decrypt(message, shift)
+    print(f"Your Decrypted message is {result}.")
+
